d1606324472a2b36c68d90bebaa6dda71a312a27
ShradddhaTJ <shraddhatj23@gmail.com>
2021-08-11 23:55:46 -0700

    check deployment pipeline

--- src/Homepage.js
+++ src/Homepage.js
@@ -4,11 +4,11 @@ class Homepage extends Component {
   render(){
     return(
       <div>
+      <h1>Fake news</h1>
       <h1>CONSULTECH ENGINEERS</h1>
 <h1>Corporate Training Services</h1>
 	<h1>Contact Us :    ceo@consultechengineers.in		  training@consultechengineers.in </h1>
 	<h1>Quick Response Number:  +91 9096545201 (Mobile & WA)     ;   +91 9881717344</h1>
-
       </div>
     );
   }
