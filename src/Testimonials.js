2f4881d39c2153da43e0eb3bfc84c3a8e5af1385
SanatP123 <paranjapesanat@gmail.com>
2021-08-21 13:06:42 +0530

    First Commit

--- src/Testimonials.js
+++ src/Testimonials.js
@@ -4,7 +4,18 @@ class Testimonials extends Component {
   render(){
     return(
       <div>
-      <div className="masthead-heading">Testimonials</div>
+      <div className="masthead-heading"><h2 style={{color: 'blue'}}>Testimonials</h2></div>
+      <iframe width="420" height="315" src="https://www.youtube.com/embed/kauL0sAH9Fo" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+      <iframe width="420" height="315" src="https://www.youtube.com/embed/t3zN4M8yrx4" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+      <iframe width="380" height="290" src="https://www.youtube.com/embed/p83KrXMLOn8" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+      <iframe width="420" height="315" src="https://www.youtube.com/embed/BabwIBD5K_4" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+      <iframe width="420" height="315" src="https://www.youtube.com/embed/LNHkv0NeWJM" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+      <iframe width="420" height="315" src="https://www.youtube.com/embed/ULW86Vm0-bw" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+      <iframe width="420" height="315" src="https://www.youtube.com/embed/Fh0oChIoP9A" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+      <iframe width="420" height="315" src="https://www.youtube.com/embed/8K05A_N2nx0" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+      <iframe width="420" height="315" src="https://www.youtube.com/embed/6jlD7EReVRQ" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+      <iframe width="420" height="315" src="https://www.youtube.com/embed/-swjrCdP3qE" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+
       </div>
     );
   }
